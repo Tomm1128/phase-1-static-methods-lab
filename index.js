228d5eb9066358a75484ec113ba603b91d7a26f8
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-03 15:20:36 -0400

    Create capitalize and sanitize static methods

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(word){
+    return word[0].toUpperCase() + word.slice(1)
+  }
+
+  static sanitize(word){
+    return word.replace(/[^A-Za-z0-9- ']+/g, "");
+  }
+
+
 }
