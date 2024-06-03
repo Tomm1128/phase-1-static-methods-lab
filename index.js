9bc3cb99ca9041660b56b93c026e87ec37409cdb
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-03 15:57:55 -0400

    Create titleize static method

--- index.js
+++ index.js
@@ -9,5 +9,24 @@ class Formatter {
     return word.replace(/[^A-Za-z0-9- ']+/g, "");
   }
 
+  static titleize(title){
+    const nonCapitals = ["a", "an", "but", "of", "and", "for", "at", "by", "from", "the"]
+    let wordArray = title.split(" ")
+    let firstWord = wordArray[0]
+    let remainingTitle = wordArray.slice(1)
+    let newTitle = []
+    newTitle.push(this.capitalize(firstWord))
+
+    remainingTitle.forEach((word) => {
+      if (nonCapitals.includes(word)){
+        newTitle.push(word)
+      }
+      else {
+        newTitle.push(this.capitalize(word))
+      }
+    })
+
+    return newTitle.join(" ")
+  }
 
 }
